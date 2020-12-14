4b6476a084b911d44a19f3878a810e2053117d4a
Usama Taj <usama.taj@hotmail.com>
2020-12-14 21:51:51 +0500

    securing application

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3$fapwytx=#vk_8!2z0xdyopx7h-o9ddrwfo(xu=o78)w5xme='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
